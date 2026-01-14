67f114586b0311d7823f914e78e4318679cbdb99
lllmio <1689966+lllmio@users.noreply.github.com>
2026-01-14 19:58:31 +0100

    enable watchlist in non-english imdb

--- IMDb_Scout_Mod.user.js
+++ IMDb_Scout_Mod.user.js
@@ -26,6 +26,7 @@
 // @include      https://*.imdb.tld/title/tt*
 // @include      https://*.imdb.tld/search/title*
 // @include      https://*.imdb.tld/user/ur*/watchlist*
+// @include      https://*.imdb.tld/*/user/ur*/watchlist*
 // @include      https://*.imdb.tld/user/ur*/ratings*
 // @include      https://*.imdb.tld/list/ls*
 //
@@ -12841,6 +12842,7 @@ if (onReferencePage) {
 
 scoutWarning();
 scoutWarning2();
+
 
 
 
